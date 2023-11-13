845147b0217c9dc527a13f595dadb3c89912b114
DannyHsi <dannyhsi0630@gmail.com>
2023-11-14 02:08:28 +0800

    make it better 3

---
+++ text.py
@@ -0,0 +1,4 @@
+X = input("請輸入名字:")
+Y = input("請輸入課程:")
+print("歐嗨喲!{},今天課程{}介紹python".format(X,Y))
+
---
+++ web.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    X = "作者：資管二Ａ 習祐翔<br>"
+    X += "<a href=/aboutme>我的個人簡介</a><br>"
+    X += "<a href=/account>MIS相關工作介紹</a><br>"
+    X += "<a href=/today>職涯測驗結果</a><br>"
+    X += "<a href=/welcome>未來規劃</a><br>"
+    return X
+
+@app.route("/today")
+def today():
+    return render_template("today.html")
+
+@app.route("/aboutme")
+def about():
+    return render_template("aboutme.html")  
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+#if __name__ == "__main__":
+    #app.run()
